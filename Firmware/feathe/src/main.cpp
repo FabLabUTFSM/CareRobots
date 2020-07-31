e18e14672d808da3908d48f90add48c1276b3132
jtomasd <josetodf@gmail.com>
2020-07-30 22:14:57 -0400

    conceted to WiFi

--- Firmware/feathe/src/main.cpp
+++ Firmware/feathe/src/main.cpp
@@ -1,14 +1,22 @@
 #include <Arduino.h>
 #include <wifi.h>
+#include <ArduinoJson.h>
 
 // WiFi Config
 char ssid[] = "MIT";
 //char pass[] =; //Uncomment to set the network password
 int status = WL_IDLE_STATUS;
+
+//Json parameters
+const size_t capacity = JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(2) + 30;
+StaticJsonBuffer<capacity> jsonBuffer;
+
+
  
 void setup() {
+  //Serial setup
   Serial.begin(9600);
-  //Serial.println("Holi!");
+  //WiFi setup
   WiFi.begin(ssid); //Uncomment if the wifi network doesn't have a password
   //WiFi.begin(ssid,pass); //Uncomment if the wifi network has a password
   while ( status != WL_CONNECTED) {
@@ -19,8 +27,14 @@ void setup() {
       // wait 10 seconds for connection:
       delay(10000);
   }
-  Serial.print("Conected to: ");
-  Serial.println(ssid);
+  if (status== WL_CONNECT_FAILED){
+    Serial.println("Unable to connect to WiFi");
+  }
+  else
+  {
+    Serial.print("Conected to: ");
+    Serial.println(ssid);
+  }
 }
 
 void loop() {
